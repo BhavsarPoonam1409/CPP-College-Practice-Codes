ddda12af97b08ce8f5eb86c3a263a91d1c2421ec
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-09-21 09:18:42 +0530

    data types in c++

---
+++ 2_basic_datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    float b=45.89;
+    char c='i';
+    string d="name";
+    double e=67.8034365354;
+
+    cout<<"Integer Is:"<<a<<endl;
+    cout<<"Float Is:"<<b<<endl;
+    cout<<"Character is:"<<c<<endl;
+    cout<<"String is:"<<d<<endl;
+    cout<<"Double is:"<<e<<endl;
+    return 0;
+}
