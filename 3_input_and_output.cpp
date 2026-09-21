6d53421fb9af5f15c4d5b0c41ec6047c2f610645
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-09-21 09:19:30 +0530

    Input and Output in c++

---
+++ 3_input_and_output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    cout<<"Enter A and b:\n";
+    cin>>a;
+    cin>>b;
+    //cin>>a>>b;
+
+    cout<<"A is:"<<a<<"B is:"<<b;
+    return 0;
+}
