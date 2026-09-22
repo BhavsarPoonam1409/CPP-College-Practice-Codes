0434ec8352cc661aca57efc8ad4c94a3254dd416
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-09-22 09:31:53 +0530

    Loop in c++

---
+++ 5_loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter number:";
+    cin>>n;
+
+    //For Loop
+    // for(int i=0;i<=n;i++){
+    //     cout<<"i is:"<<i<<endl;
+    // }
+
+    //while loop
+    int i=0;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
